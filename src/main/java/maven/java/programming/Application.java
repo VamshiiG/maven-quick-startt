bb04cf194b0477a978ca396ca7b8a9bd0fa652bf
Vamshi G <vamshig898@gmail.com>
2023-06-03 00:05:31 +0530

    used StringUtils and added dependencies for apache commons lang3

--- src/main/java/maven/java/programming/Application.java
+++ src/main/java/maven/java/programming/Application.java
@@ -2,13 +2,19 @@ package maven.java.programming;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
-
+	
+	public int countWords(String words) {
+		String[] seperateWords = StringUtils.split(words, ' ');
+		return (seperateWords == null) ? 0 : seperateWords.length;
+	}
+	
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello ... ");
@@ -22,5 +28,8 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("aa bb cc dd");
+		System.out.println("words count : "+count);
     }
+	
 }
